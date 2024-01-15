8dd6c3736dfbaca250ad5bb8b21f86b41b1ae2df
Qbert <frc1736student@gmail.com>
2024-01-15 16:41:57 -0600

    Still a WIP (need to find a place to actually use the alliance transform class). But this should successfully revert Linux's oopsy commit that pushed onto main. Shoutout to big Elliot for his unending knowledge

--- utils/allianceTransformUtils.py
+++ utils/allianceTransformUtils.py
@@ -3,10 +3,10 @@ from wpilib import DriverStation
 from hal import AllianceStationID
 
 from wpimath.geometry import Pose2d, Rotation2d, Transform2d, Translation2d
-
-from utils.constants import FIELD_LENGTH_FT
-
-from utils.units import m2ft
+from wpimath.geometry import Pose2d, Rotation2d, Transform2d, Translation2d
+from wpimath.geometry import Pose2d, Rotation2d, Transform2d, Translation2d
+from wpimath.geometry import Pose2d, Rotation2d, Transform2d, Translation2d
+from wpimath.geometry import Pose2d, Rotation2d, Transform2d, Translation2d
 
 """
  Utilities to help transform from blue alliance to red if needed
@@ -14,15 +14,47 @@ from utils.units import m2ft
  bottom left on the blue alliance
 """
 class AllianceTransformUtils:
-
-
     @staticmethod
-    def transformX(input):
-        if DriverStation.getAlliance() == AllianceStationID.Red: # type: ignore
-            return m2ft(FIELD_LENGTH_FT) - input
+    def transformX( self, input):
+        
+        if wpilib._wpilib.DriverStation.Alliance == Alliance.kRed:
+            return 16.4592 - input
         else:
             return input
     
-    def transformY(input):
+    def transformY(self,input):
         return input
+    
+    def Rotation2d(self,input):
+        if wpilib._wpilib.DriverStation.Alliance == Alliance.kRed:
+            return Rotation2d.fromDegrees(180).minus(input)
+        else: 
+            return input 
 
+    def Translation2d(self,input):
+        if wpilib._wpilib.DriverStation.Alliance == Alliance.kRed:
+            return Translation2d(AllianceTransformUtils.transformX(input.getX()), input.getY())
+        else:
+            return input
+    
+    def Transform2d(self,input):
+        if wpilib._wpilib.DriverStation.Alliance == Alliance.kRed:
+            trans = AllianceTransformUtils.transform(input.getTranslation())
+            rot = AllianceTransformUtils.transform(input.getRotation())
+            return Transform2d(trans, rot)
+        else:
+            return input
+        
+    def Pose2d(self,input):
+        if wpilib._wpilib.DriverStation.Alliance == Alliance.kRed:
+            trans = AllianceTransformUtils.transform(input.getTranslation())
+            rot = AllianceTransformUtils.transform(input.getRotation())
+            return Pose2d(trans, rot)
+        else:
+            return input
+    
+    def transformChoreoTrajectoryState(self,input):
+        if wpilib._wpilib.DriverStation.Alliance == Alliance.kRed:
+            return input.flipped()
+        else:
+            return input
