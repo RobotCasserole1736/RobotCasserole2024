5e2082364c6e666002495ae3de8097fbf41bdef4
Chris Gerth <chrisgerth010592@gmail.com>
2024-01-17 18:35:35 -0600

    WIP skeleton

---
+++ humanInterface/ledControl.py
@@ -0,0 +1,19 @@
+
+
+
+class LEDControl():
+
+    def __init__(self):
+        # Put any one-time init code here
+        pass
+
+    def update(self):
+        # Put any code that runs every periodic loop here
+        pass
+
+    
+    def setSpeakerAutoAlignActive(self, isActive):
+        pass # TODO - do something with the active status
+
+    def setNoteInIntake(self, hasNote):
+        pass # TODO - do something with hasNote
