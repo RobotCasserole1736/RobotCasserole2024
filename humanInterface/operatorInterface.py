86597aeb7de62fc31655eaa8ed6368e00523697d
Qbert <frc1736student@gmail.com>
2024-01-17 20:31:55 -0600

    Started skeleton code. WIP

--- humanInterface/operatorInterface.py
+++ humanInterface/operatorInterface.py
@@ -1,1 +1,18 @@
-#Need operator controls
+#imports
+
+def __init__(self):
+    #initialize xbox controller, important values
+    #is the position commands going to be a enum later? I think they should be. 
+    #But I also think that right now, they're booleans anyways. You don't change them to an enum until later? 
+    #At least we didn't with shooter positions last year
+    pass
+
+def update(self):
+    #update the values from the xbox controller. Updates every 20(?)ms
+    #Make sure there's logic for if a controller is connected and nothing happens if it doesn't
+    pass
+
+
+def getAutoAlignCmd(self):
+    #returns whether auto align is desired or not
+    pass
