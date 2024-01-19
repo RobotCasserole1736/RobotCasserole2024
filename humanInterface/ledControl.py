a91f57ee12e36fa82341dc857d6be617fc376661
Chris Gerth <chrisgerth010592@gmail.com>
2024-01-18 19:18:31 -0600

    Added code to support LEDCtrl

--- humanInterface/ledControl.py
+++ humanInterface/ledControl.py
@@ -1,19 +1,40 @@
-
+from wpilib import PWMMotorController
 
 
 class LEDControl():
 
     def __init__(self):
         # Put any one-time init code here
+        self.speakerAutoAlignActive = False
+        self.noteInIntake = False
+        self._noteInIntakePrev = False
+        self._noteInIntakeCounter = 0
+        self.ctrl = PWMMotorController("LEDCtrl", 9)
+        self._SAMPLE_TIME = 0.02
         pass
 
     def update(self):
-        # Put any code that runs every periodic loop here
-        pass
+        pwmVal = 0.0 # default = off
+
+        if(self.noteInIntake and not self._noteInIntakePrev):
+            self._noteInIntakeCounter = int(1.0 / self._SAMPLE_TIME) # Set LED's to blink for 1 second
+
+        if(self.speakerAutoAlignActive):
+            pwmVal = 0.5 # Blue Solid
+        elif(self._noteInIntakeCounter > 0):
+            pwmVal = -0.35# Green Blink
+            self._noteInIntakeCounter -= 1
+
+        self.ctrl.set(pwmVal)
+
+
+        self._noteInIntakePrev = self.noteInIntake
+
+        
 
     
     def setSpeakerAutoAlignActive(self, isActive):
-        pass # TODO - do something with the active status
+        self.speakerAutoAlignActive = isActive
 
     def setNoteInIntake(self, hasNote):
-        pass # TODO - do something with hasNote
+        self.noteInIntake = hasNote
