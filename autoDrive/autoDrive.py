0ca6739e2705c1ac40a41678f112213ac37a5649
LucasT06 <lwtanner06@gmail.com>
2024-01-20 08:36:18 -0600

    Add "skeleton" code to autoDrive.py

--- autoDrive/autoDrive.py
+++ autoDrive/autoDrive.py
@@ -1,1 +1,12 @@
 #auto drive stuff
+
+import wpilib
+from wpimath.geometry import Pose2d, Rotation2d, Transform3d, Twist2d
+from drivetrain.drivetrainControl import DrivetrainControl
+
+
+
+class autoDrive():
+    def __init__(self):
+        pass
+
