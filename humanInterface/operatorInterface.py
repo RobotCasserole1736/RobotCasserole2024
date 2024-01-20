6fb81703fbb00808ef17ada9f408187c39c7e686
Qbert <frc1736student@gmail.com>
2024-01-20 13:45:22 -0600

    Added a manual option for moving the carriage/elevator up and down, as well as rotating the singer. It's not used anywhere yet
    Added commands for the shooting positions for the carriage from the subwoofer/speaker and from the podium. These positions are supposed to be the right and left sides ish of the dpad. I'm not sure how touchy the dpad is, though. Do we need a deadband?

--- humanInterface/operatorInterface.py
+++ humanInterface/operatorInterface.py
@@ -1,7 +1,11 @@
 #imports
 from typing import Any
 from wpilib import XboxController
+from wpimath import applyDeadband
 from utils.faults import Fault
+from wpimath.filter import SlewRateLimiter
+
+from utils.units import in2m
 
 class operatorInterface:
     def __init__(self):
@@ -26,11 +30,23 @@ class operatorInterface:
         #element of the elevator. Goes up, down, and rotates into position
         self.carriageIntakePos = False
         self.carriageAmpPos = False
-        self.carriageSpeakerPos = False
         self.carriageTrapPos = False
+        self.carriagePodiumPos = False
+        self.carriageSpeakerSubwooferPos = False
 
         #if the operator wants the auto align desired
         self.AutoAlignDesired = False
+
+        #singer manual controls
+        self.manualSingerUpDown = False
+        self.manualSingerRot = False
+
+        #I don't know what the max on the slew rate limiter should be. It should be a constant
+        #I just made up a number... it happened to be last year's number. I just picked something
+        MAX_MAN_VEL_MPS = in2m(12.0)
+        self.manualSingerUpDownSlewRateLimiter = SlewRateLimiter(MAX_MAN_VEL_MPS)
+        self.manualSingerRotSlewRateLimiter = SlewRateLimiter(MAX_MAN_VEL_MPS)
+
 
 
 
@@ -48,11 +64,20 @@ class operatorInterface:
             #element of the elevator. Goes up, down, and rotates into position
             self.carriageIntakePos = self.ctrl.getAButton()
             self.carriageAmpPos = self.ctrl.getBButton()
-            self.carriageSpeakerPos = self.ctrl.getXButton()
             self.carriageTrapPos = self.ctrl.getYButton()
+            self.carriageSpeakerSubwooferPos = 225 < self.ctrl.getPOV() < 315
+            #Above is basically the left side of the D pad
+            self.carriagePodiumPos = 45 < self.ctrl.getPOV() < 135
+            #Above is basically the right side of the D pad
 
             #if the operator wants the auto align desired
             self.AutoAlignDesired = self.ctrl.getXButton()
+
+            #manual singer controls
+            self.manualSingerUpDown = applyDeadband(self.ctrl.getLeftY(),.15)
+            self.manualSingerUpDown = self.manualSingerUpDownSlewRateLimiter.calculate(self.manualSingerUpDown)
+            self.manualSingerRot = applyDeadband(self.ctrl.getRightX(),.15)
+            self.manualSingerRot = self.manualSingerRotSlewRateLimiter.calculate(self.manualSingerRot)
 
             self.connectedFault.setNoFault()
 
@@ -67,11 +92,16 @@ class operatorInterface:
             #element of the elevator. Goes up, down, and rotates into position
             self.carriageIntakePos = False
             self.carriageAmpPos = False
-            self.carriageSpeakerPos = False
             self.carriageTrapPos = False
+            self.carriageSpeakerSubwooferPos = False
+            self.carriagePodiumPos = False
 
             #if the operator wants the auto align desired
             self.AutoAlignDesired = False
+
+            #manual commands
+            self.manualSingerUpDown = 0
+            self.manualSingerRot = 0
 
 
  #and now a bunch of functions to call 
@@ -116,15 +146,23 @@ class operatorInterface:
         #returns whether the singer is being commanded go to amp position
         return self.carriageAmpPos
     
-    def getCarriageSpeakerPosCmd(self):
-        #returns whether the singer is being commanded go to the position it will need to shoot? But from where?
-        return self.carriageSpeakerPos
-    
     def getCarriageTrapPosCmd(self):
         #returns whether the singer is being commanded go to the position it will need for trap
         return self.carriageTrapPos
+    
+    def getCarriageSpeakerSubwooferPosCmd(self):
+        #returns whether the singer is being commanded go to the position it will need to shoot from the subwoofer of the speaker
+        return self.carriageSpeakerSubwooferPos
+    
+    def getCarriagePodiumPosCmd(self):
+        #returns whether the singer is being commanded go to the position it will need to shoot from the podium
+        return self.carriagePodiumPos
     
     #when you're using this logic, make sure there is an "else" if none of these commands are activated. 
     #In the else:, nothing should happen
 
     """We will use these getters with other motor-related setters, once those have been created"""
+
+    #if the manual commands to control the singer are active, we will have to know that and get them later
+    def manCmdActive(self):
+        return  self.manualSingerUpDown != 0 or self.manualSingerRot != 0
