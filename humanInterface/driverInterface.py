57f0d159993d71a441bbf1f6e8c6a053aad48e64
Skywalker <frc1736student@gmail.com>
2024-01-23 19:34:09 -0600

    fixed lint errors

--- humanInterface/driverInterface.py
+++ humanInterface/driverInterface.py
@@ -4,8 +4,6 @@ from wpimath.filter import SlewRateLimiter
 from drivetrain.drivetrainPhysical import MAX_FWD_REV_SPEED_MPS
 from drivetrain.drivetrainPhysical import MAX_STRAFE_SPEED_MPS
 from drivetrain.drivetrainPhysical import MAX_ROTATE_SPEED_RAD_PER_SEC
-from drivetrain.drivetrainPhysical import MAX_ROTATE_ACCEL_RAD_PER_SEC_2
-from drivetrain.drivetrainPhysical import MAX_TRANSLATE_ACCEL_MPS2
 from utils.faults import Fault
 from utils.signalLogging import log
 
@@ -14,47 +12,47 @@ class DriverInterface:
 
     def __init__(self):
         # contoller 
-        CtrlIdx = 0
-        self.ctrl = XboxController(CtrlIdx)
+        ctrlIdx = 0
+        self.ctrl = XboxController(ctrlIdx)
         self.vXCmd = 0
         self.vYCmd = 0
         self.vRotCmd = 0
-        self.gryoResetCmd = False
+        self.gyroResetCmd = False
         
         #Slew rate limiter
-        self.VX_SRL = SlewRateLimiter(2)
-        self.VY_SRL = SlewRateLimiter(2)
-        self.VRot_SRL = SlewRateLimiter(8)
+        self.vXSlewRateLimiter = SlewRateLimiter(2)
+        self.vYSlewRateLimiter = SlewRateLimiter(2)
+        self.vRotSlewRateLimiter = SlewRateLimiter(8)
         
-        self.connectedfault = Fault(f"Driver XBox Controller ({CtrlIdx})unplugged")
+        self.connectedfault = Fault(f"Driver XBox Controller ({ctrlIdx})unplugged")
 
     def update(self):
         # value of contoller buttons
 
-        if self.ctrl.isConnected:
-            VXJoyRaw = self.ctrl.getLeftY() * -1
-            VYJoyRaw = self.ctrl.getLeftX() * -1
-            VRotJoyRaw = self.ctrl.getRightX() * -1
+        if self.ctrl.isConnected():
+            vXJoyRaw = self.ctrl.getLeftY() * -1
+            vYJoyRaw = self.ctrl.getLeftX() * -1
+            vRotJoyRaw = self.ctrl.getRightX() * -1
    
             #deadband
-            VXJoy = applyDeadband(VXJoyRaw,0.15)
-            VYJoy = applyDeadband(VYJoyRaw,0.15)
-            VRotJoy = applyDeadband(VRotJoyRaw,0.15)
+            vXJoy = applyDeadband(vXJoyRaw,0.15)
+            vYJoy = applyDeadband(vYJoyRaw,0.15)
+            vRotJoy = applyDeadband(vRotJoyRaw,0.15)
 
             slowMult = .5 if (self.ctrl.getRightBumper()) else 1.0
 
             #velocity cmd
-            vel_CmdXRaw = VXJoy * MAX_STRAFE_SPEED_MPS * slowMult
-            vel_CmdYRaw = VYJoy * MAX_FWD_REV_SPEED_MPS * slowMult
-            vel_CmdRotRaw = VRotJoy * MAX_ROTATE_SPEED_RAD_PER_SEC
+            velCmdXRaw = vXJoy * MAX_STRAFE_SPEED_MPS * slowMult
+            velCmdYRaw = vYJoy * MAX_FWD_REV_SPEED_MPS * slowMult
+            velCmdRotRaw = vRotJoy * MAX_ROTATE_SPEED_RAD_PER_SEC
 
             # Slew rate limiter
-            self.vXCmd = self.VX_SRL.calculate(vel_CmdXRaw)
-            self.vYCmd = self.VY_SRL.calculate(vel_CmdYRaw)
-            self.vRotCmd = self.VRot_SRL.calculate(vel_CmdRotRaw)
+            self.vXCmd = self.vXSlewRateLimiter.calculate(velCmdXRaw)
+            self.vYCmd = self.vYSlewRateLimiter.calculate(velCmdYRaw)
+            self.vRotCmd = self.vRotSlewRateLimiter.calculate(velCmdRotRaw)
         
 
-            self.GyroResetCmd = self.ctrl.getAButtonPressed()
+            self.gyroResetCmd = self.ctrl.getAButtonPressed()
 
             self.connectedfault.setNoFault()
 
@@ -94,4 +92,4 @@ class DriverInterface:
         pass
 
     def getGyroResetCmd(self):
-        return self.gryoResetCmd
+        return self.gyroResetCmd
