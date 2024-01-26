c598e663467fdd00713a5c8dcf5dac3daddf44d6
Baymax <frc1736student@gmail.com>
2024-01-25 19:59:37 -0600

    added new stuff for tof

--- gamepieceHandling/gamepieceHandling.py
+++ gamepieceHandling/gamepieceHandling.py
@@ -1,26 +1,28 @@
 #Priorities
-#1. Read the rest of the documentation for ToF 
-#2. Take a look at SparkMax stuff
-#3. Ask about playing with fusion and how to implement and if did classes correct
-#4. Get information from Operator control block on architecture
-
-#Resources: https://robotpy.readthedocs.io/projects/pwfusion/en/latest/playingwithfusion/TimeOfFlight.html
+#Finish ToF sensor stuff in init
 
 
-#import all we need
+
 from wpilib import PWMSparkMax
 from utils.calibration import Calibration
-from utils import constants
-from utils.units import RPM2RadPerSec
+from utils import constants, faults
+from utils.units import RPM2RadPerSec, m2in
 from wrappers.wrapperedSparkMax import WrapperedSparkMax
 from playingwithfusion import TimeOfFlight
 
 
 class gamepieceHandling:
     def __init__(self):
-
+        
+        self.hasGamePiece = False
         #Time of flight sensor
-        self.ToFSensor = ()
+        #Finish ToF sensor
+        self.ToFSensor = TimeOfFlight(16) #? Don't know what the ID is right now
+        self.ToFSensor.setRangingMode(TimeOfFlight.RangingMode.kShort, 24) # 24 is 2023 values
+        self.ToFSensor.setRangeOfInterest(6, 6, 10, 10) # fov for sensor (2023 code)
+        
+        #fault
+        self.disconTOFFault = faults.Fault("Shooter TOF Sensor", "Disconnected")
 
         #Shooter Motors
         self.Shooter1 = WrapperedSparkMax(constants.SHOOTER_MOTOR1_CANID, "ShooterMotor1")
@@ -33,11 +35,14 @@ class gamepieceHandling:
         #Floor Roller Motors
         self.FloorRoller1 = WrapperedSparkMax(constants.FlOORROLLER_MOTOR1_CANID, "FloorRollerMotor1")
         self.FloorRoller2 = WrapperedSparkMax(constants.FLOORROLLER_MOTOR2_CANID, "FloorRollerMotor2")
-
         
-        # Calibrations for Shooter (PID Controller)
+        # Calibrations for Shooter
         self.ShooterkF = Calibration("ShooterkF", 0.00255, "V/RPM")
         self.ShooterkP = Calibration("ShooterkP", 0)
+
+        #Calibrations for Gamepiece being absent and present
+        self.GamePiecePresent = Calibration("Note/Gamepiece", "in", 7) #2023 values 
+        self.GamePieceAbsent = Calibration("Note/Gamepiece", "in", 11) #2023 values
 
         #Voltage Calibration
         self.IntakeVoltage = Calibration("IntakeVoltage", 12, "V")
@@ -54,16 +59,28 @@ class gamepieceHandling:
         self.FloorRoller1.setVoltage(self.IntakeVoltage)
         self.FloorRoller2.setVoltage(self.IntakeVoltage)
 
-    
+    def update(self):
+        gamepieceDistSensorMeas = m2in(self.ToFSensor.getRange()/1000.0)
+        self.disconTOFFault.set(self.ToFSensor.getFirmwareVersion() == 0)
+
+
+        if (gamepieceDistSensorMeas < self.GamePiecePresent.get()):
+            self.hasGamePiece = True
+        elif(gamepieceDistSensorMeas > self.GamePieceAbsent.get()):
+            self.hasGamePiece = False
+        else:
+            pass
+        
+
    
 
         # SparkMax motor for the floor roller
 
 
-    def getOperatorControlXboxControl():
-        pass
+    #def getOperatorControlXboxControl():
+     #   pass   
 
 
 
 
-    # What the "note flow control" has to get, which is the xbox control, from the Operator Control node.
+    # What the "note flow control" has to get, which is the xbox control, from the Operator Control node
