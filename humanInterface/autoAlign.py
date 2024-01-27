23170b5b142cb24f5a73605b633a631ddf44e254
Huey <frc1736student@gmail.com>
2024-01-26 20:21:07 -0600

    Co-authored-by: Tyler Krause <gojira2021@users.noreply.github.com>

--- humanInterface/autoAlign.py
+++ humanInterface/autoAlign.py
@@ -16,16 +16,22 @@ class AutoAlign():
         
         #returnVal = (3.1415/2) - self.AARobotPoseEst.rotation().radians()# + math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X() ) #
 
-        returnVal =math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) - self.AARobotPoseEst.rotation().radians()# self.AARobotPoseEst.rotation().radians() - math.atan(  self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) + (3.1415/2)
+        #returnVal = ( 2*math.pi +math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) - 3.1415 ) - (self.AARobotPoseEst.rotation().radians() )# self.AARobotPoseEst.rotation().radians() - math.atan(  self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) + (3.1415/2)
+        returnVal = ( math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) - 3.1415 ) % (2*math.pi) - (self.AARobotPoseEst.rotation().radians() )
+        if abs(returnVal) > math.pi:
+            returnVal = ((2* math.pi) - returnVal) * -1
+        
 
         if returnVal > 0:
             returnVal = 1
         elif returnVal < 0:
             returnVal = -1
 
+        print(f"current:{math.degrees(self.AARobotPoseEst.rotation().radians())} wanted: {math.degrees(2*math.pi + math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) - 3.1415)}")
+        
 
-        print(f"X:{self.AARobotPoseEst.X()} Y: {self.AARobotPoseEst.Y()} rotation: {self.AARobotPoseEst.rotation().radians()}")
-        print(returnVal)
+        #print(f"X:{self.AARobotPoseEst.X()} Y: {self.AARobotPoseEst.Y()} rotation: {self.AARobotPoseEst.rotation().radians()}")
+        #print(returnVal)
         
         return returnVal 
     
