64b48e528c6b373e35ef60a6fe0aa839e10290e4
Huey <frc1736student@gmail.com>
2024-01-26 20:32:31 -0600

    WIP - AA now follows fastest path to point at target. Look at code comments on lines 24 and 25 of autoallign.py for more info on how it works

--- humanInterface/autoAlign.py
+++ humanInterface/autoAlign.py
@@ -17,10 +17,12 @@ class AutoAlign():
         #returnVal = (3.1415/2) - self.AARobotPoseEst.rotation().radians()# + math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X() ) #
 
         #returnVal = ( 2*math.pi +math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) - 3.1415 ) - (self.AARobotPoseEst.rotation().radians() )# self.AARobotPoseEst.rotation().radians() - math.atan(  self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) + (3.1415/2)
-        returnVal = ( math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) - 3.1415 ) % (2*math.pi) - (self.AARobotPoseEst.rotation().radians() )
+        returnVal = ( math.atan((self.AARobotPoseEst.Y() - 2)/(self.AARobotPoseEst.X() - 2)) - 3.1415 ) % (2*math.pi) - (self.AARobotPoseEst.rotation().radians() )
         if abs(returnVal) > math.pi:
             returnVal = ((2* math.pi) - returnVal) * -1
         
+        #We need to detect if we are on the left of the point and if we are dont subtract by pi. look at the board for how to change the point that the robot is pointing at
+        #currently pointing at a position 2 units above and 2 units to the right of (0,0)
 
         if returnVal > 0:
             returnVal = 1
