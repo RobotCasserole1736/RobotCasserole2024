678cfbe0eaf52703063f525330fd9806dbdfb346
Huey <frc1736student@gmail.com>
2024-02-01 20:42:52 -0600

    Working horizontal auto allignment, could improve It does work and does minimized overshoot, but it could work faster and technichally never reaches the angle it wants, just gets so close that the difference is really really small.

--- drivetrain/controlStrategies/autoDrive.py
+++ drivetrain/controlStrategies/autoDrive.py
@@ -1,6 +1,8 @@
 import math
 from wpimath.geometry import Pose2d
 from drivetrain.drivetrainCommand import DrivetrainCommand
+from utils.allianceTransformUtils import onRed
+from utils.allianceTransformUtils import transform
 from utils.singleton import Singleton
 
 
@@ -20,58 +22,72 @@ class AutoDrive(metaclass=Singleton):
 
     def update(self, cmdIn: DrivetrainCommand, curPose: Pose2d) -> DrivetrainCommand:
         if self.active:
-            self.speakerAlign(
-                curPose
-            )  # TODO - this needs to return a DrivetrainCommand
-
+              # TODO - this needs to return a DrivetrainCommand
+            
             return (
-                DrivetrainCommand()
+                self.speakerAlign(
+                curPose
+                )   
             )  # TODO - this drivetrain command is just "don't move", needs to be something else
         else:
             return cmdIn
 
     def speakerAlign(self, curPose):
         self.AARobotPoseEst = curPose
+        
+        ### NOTICE ###
+        #This is a butchered version of the code thats a mix between Chris' version and my (Kyle's) most recent updated veersion. It might not work and comments are not updated. Large amounts of code are currently commented out instead of being deleted. Use at your own risk.
+        if onRed() == True:
+            self.targetX = 16.54175 - 0.22987
+            self.targetY = 5.4572958333417994
+        else:
+            self.targetX = 0.22987#targetX
+            self.targetY = 5.4572958333417994
 
-        # For now I am assuming that you are on the blue alliance
+        #For now I am assuming that you are on the blue alliance
+        
+        
 
-        # if self.AARobotPoseEst.rotation().radians() <0:
+        print("Doing stuff right now :)")
+        #I am currently declairing these as test variables. They store the target X and Y.
+        
 
-        # returnVal = (3.1415/2) - self.AARobotPoseEst.rotation().radians()
-        # + math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X() ) #
-
-        # returnVal = ( 2*math.pi +math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) - 3.1415 ) 
-        # - (self.AARobotPoseEst.rotation().radians() )# self.AARobotPoseEst.rotation().radians() 
-        # - math.atan(  self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) + (3.1415/2)
-        returnVal = (
-            math.atan((self.AARobotPoseEst.Y() - 2) / (self.AARobotPoseEst.X() - 2))
-            - 3.1415
-        ) % (2 * math.pi) - (self.AARobotPoseEst.rotation().radians())
+        if self.AARobotPoseEst.X() - self.targetX > 0: # test to see if we are to the right of the robot
+            #If we are, we have to correct the angle by 1 pi
+            returnVal = ( math.atan((self.AARobotPoseEst.Y() - self.targetY)/(self.AARobotPoseEst.X() - self.targetX)) - math.pi ) % (2*math.pi) - (self.AARobotPoseEst.rotation().radians() )
+        else:
+            #If we aren't, we don't need to.
+            returnVal = ( math.atan((self.AARobotPoseEst.Y() - self.targetY)/(self.AARobotPoseEst.X() - self.targetX)) ) % (2*math.pi) - (self.AARobotPoseEst.rotation().radians() )
+        
+        #Test if the angle we calculated will be greater than 180 degrees. If it is, reverse it.
         if abs(returnVal) > math.pi:
-            returnVal = ((2 * math.pi) - returnVal) * -1
+            returnVal = ((2* math.pi) - returnVal) * -1
+        
+        #We need to detect if we are on the left of the point and if we are dont subtract by pi. look at the board for how to change the point that the robot is pointing at
+        #currently pointing at a position 2 units above and 2 units to the right of (0,0)
 
-        # We need to detect if we are on the left of the point and if we are dont subtract by pi. look at the board for how to change the point that the robot is pointing at
-        # currently pointing at a position 2 units above and 2 units to the right of (0,0)
 
-        if returnVal > 0:
-            returnVal = 1
+        #We will need to update this to make it faster. It moves according to if the angle difference is positive or negative.
+
+        """if returnVal > 0:
+            returnVal = 2
         elif returnVal < 0:
-            returnVal = -1
+            returnVal = -2"""
+        
+        
+        #self.XVelAvg = 1#(self.AARobotInstance.getModuleSpeeds()[0])# + self.AARobotInstance.getModuleSpeeds()[1].X() + self.AARobotInstance.getModuleSpeeds()[2].X() + self.AARobotInstance.getModuleSpeeds()[3].X())/3
+        #self.YVelAvg = 1#(self.AARobotInstance.getModuleSpeeds()[0])# + self.AARobotInstance.getModuleSpeeds()[1].Y() + self.AARobotInstance.getModuleSpeeds()[2].Y() + self.AARobotInstance.getModuleSpeeds()[3].Y())/3
+        
+        returnDriveTrainCommand = DrivetrainCommand()
+        returnDriveTrainCommand.velT = returnVal
+        return returnDriveTrainCommand
+        #self.update(DrivetrainCommand(), curPose)
+        #self.AARobotInstance.setCmdFieldRelative(self.XVelAvg, self.YVelAvg, returnVal)
 
-        print(
-            f"current:{math.degrees(self.AARobotPoseEst.rotation().radians())} wanted: {math.degrees(2*math.pi + math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) - 3.1415)}"
-        )
-
-        # print(f"X:{self.AARobotPoseEst.X()} Y: {self.AARobotPoseEst.Y()} rotation: {self.AARobotPoseEst.rotation().radians()}")
-        # print(returnVal)
-
-        return returnVal
-import math
-from drivetrain.poseEstimation.drivetrainPoseEstimator import DrivetrainPoseEstimator
-from drivetrain.drivetrainControl import DrivetrainControl
-
-class AutoAlign():
-
+        return 0
+    
+#Old code, keeping it around just for reference:
+    """
     def speakerAlign(self,targetX,targetY):
         
         self.AARobotInstance = DrivetrainControl()
@@ -111,14 +127,14 @@ class AutoAlign():
         self.XVelAvg = (self.AARobotInstance.getModuleSpeeds()[0])# + self.AARobotInstance.getModuleSpeeds()[1].X() + self.AARobotInstance.getModuleSpeeds()[2].X() + self.AARobotInstance.getModuleSpeeds()[3].X())/3
         self.YVelAvg = (self.AARobotInstance.getModuleSpeeds()[0])# + self.AARobotInstance.getModuleSpeeds()[1].Y() + self.AARobotInstance.getModuleSpeeds()[2].Y() + self.AARobotInstance.getModuleSpeeds()[3].Y())/3
 
-        self.AARobotInstance.setCmdFieldRelative(self.XVelAvg, self.YVelAvg, returnVal)
+        #self.AARobotInstance.setCmdFieldRelative(self.XVelAvg, self.YVelAvg, returnVal)
         #Emergency debuging print, delete hash in case of bug related emergency:
         #print(f"current:{math.degrees(self.AARobotPoseEst.rotation().radians())} wanted: {math.degrees(2*math.pi + math.atan(self.AARobotPoseEst.Y()/self.AARobotPoseEst.X()) - 3.1415)}")
         
 
         
         
-        return 0
+        return 0"""
     
 
         
