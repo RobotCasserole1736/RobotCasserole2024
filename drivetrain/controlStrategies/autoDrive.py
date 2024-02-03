2785dec4ba1407a7319971ac3a49c5e5bf195340
Elliot Watkins <frc1736student@gmail.com>
2024-02-03 13:28:03 -0600

    Some cleanup for autoDrive.py

--- drivetrain/controlStrategies/autoDrive.py
+++ drivetrain/controlStrategies/autoDrive.py
@@ -2,7 +2,6 @@ import math
 from wpimath.geometry import Pose2d
 from drivetrain.drivetrainCommand import DrivetrainCommand
 from utils.allianceTransformUtils import onRed
-from utils.allianceTransformUtils import transform
 from utils.singleton import Singleton
 
 
@@ -10,6 +9,7 @@ class AutoDrive(metaclass=Singleton):
     def __init__(self):
         self.active = False
         self.AARobotPoseEst = None
+        self.returnDriveTrainCommand = DrivetrainCommand()
 
     def setCmd(self, shouldAutoAlign):
         """Automatically point the drivetrain toward the speaker
@@ -22,20 +22,13 @@ class AutoDrive(metaclass=Singleton):
 
     def update(self, cmdIn: DrivetrainCommand, curPose: Pose2d) -> DrivetrainCommand:
         if self.active:
-            
-            
-            return (
-                self.speakerAlign(
-                curPose, cmdIn
-                )   
-            )  
+            return (self.speakerAlign(curPose, cmdIn))
         else:
             return cmdIn
 
     def speakerAlign(self, curPose,cmdIn):
         self.AARobotPoseEst = curPose
-        
-        
+
         #Find out if we are on red team
         if onRed() == True:#If we are, set the target pos to the pos of the red speaker
             self.targetX = 16.54175 - 0.22987
@@ -43,12 +36,6 @@ class AutoDrive(metaclass=Singleton):
         else: #If we aren't, set the target pos to the pos of the blue speaker
             self.targetX = 0.22987
             self.targetY = 5.4572958333417994
-
-
-
-        
-        
-        
 
         if self.AARobotPoseEst.X() - self.targetX > 0: # test to see if we are to the right of the robot
             #If we are, we have to correct the angle by 1 pi. This is built into the following equation
@@ -60,18 +47,11 @@ class AutoDrive(metaclass=Singleton):
         #Test if the angle we calculated will be greater than 180 degrees. If it is, reverse it.
         if abs(returnVal) > math.pi:
             returnVal = ((2* math.pi) - returnVal) * -1
-        
-        
 
         if abs(returnVal) <= 0.05: #Check to see if we are making a really small correction. if we are, don't worry about it. We only need a certain level of accuracy.
             returnVal = 0
-        
 
-
-        returnDriveTrainCommand = DrivetrainCommand() #We create an instance of DrivetrainCommand and configure it.
-        returnDriveTrainCommand.velT = returnVal * 5 #set the rotational vel to 5 * the angle we calculated. We multiply it by 5 so its faster :o
-        returnDriveTrainCommand.velX = cmdIn.velX #set the X vel to the original X vel.
-        returnDriveTrainCommand.velY = cmdIn.velY #Set the Y vel to the original Y vel.
-        return returnDriveTrainCommand
-        
-    
+        self.returnDriveTrainCommand.velT = returnVal * 5 #set the rotational vel to 5 * the angle we calculated. We multiply it by 5 so its faster :o
+        self.returnDriveTrainCommand.velX = cmdIn.velX #set the X vel to the original X vel.
+        self.returnDriveTrainCommand.velY = cmdIn.velY #Set the Y vel to the original Y vel.
+        return self.returnDriveTrainCommand
