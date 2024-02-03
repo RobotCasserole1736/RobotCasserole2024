35a34c1a8f2ad85e8195710085568a2429caf38b
Oryx. The Taken King <frc1736student@gmail.com>
2024-02-03 09:52:01 -0600

    Moved most recent carriage control code into merged branch.

--- singerMovement/carriageControl.py
+++ singerMovement/carriageControl.py
@@ -1,7 +1,11 @@
 #this will be in distance along the elevator, with 0 being at bottom and the top being whatever it is
+from wpilib import Timer
 from singerMovement.singerConstants import GEARBOX_GEAR_RATIO, MAX_CARRIAGE_ACCEL_MPS2, MAX_CARRIAGE_VEL_MPS, MAX_SINGER_ROT_ACCEL_DEGPS2, MAX_SINGER_ROT_VEL_DEG_PER_SEC, SPROCKET_MULTPLICATION_RATIO
 from wrappers.wrapperedSparkMax import WrapperedSparkMax
 from utils.calibration import Calibration
+from wpimath.geometry import Pose2d
+from drivetrain.poseEstimation.drivetrainPoseEstimator import DrivetrainPoseEstimator
+from wpimath.trajectory import TrapezoidProfile
 
 class CarriageControl:
 
@@ -16,31 +20,33 @@ class CarriageControl:
         self.kMaxVRot = Calibration(name="Max Vel of Carriage singer rot", default=MAX_SINGER_ROT_VEL_DEG_PER_SEC)
         self.kMaxARot = Calibration(name="Max Acceleration of Carriage singer rot", default=MAX_SINGER_ROT_ACCEL_DEGPS2)
 
+        self.constraints = TrapezoidProfile.Constraints(maxVelocity=self.kMaxVUpDown.get(), maxAcceleration=self.kMaxAUpDown.get())
+
+        #self.curSetpoint = can't be drivetrain because it's just carriage?
+        self.prevProfiledSetpoint = DrivetrainPoseEstimator.getCurEstPose
+
 
 
     def update(self):
         pass
-
-        #up/down
-    
-        #rot
         """
-        curSetpoint = ... #  comes in from other code to say where this thing should be physically (in units of meters or degrees or whatever)
-
+        #up/down
+        #curSetpoint = can't be drivetrain setpoint because it's carriage?
         curTime = Timer.getFPGATimestamp()
 
-        if(prevSetpoint != curSetpoint):
-        #New setpoint, need to recalcualte the trajectory
-        constraints = TrapezoidProfile.Constraints(self.kMaxV, self.kMaxA)
-        profile = TrapezoidProfile(constraints, curSetpoint, self.previousProfiledSetpoint)
-        self.profileStartTime = curTime 
+        if(self.prevProfiledSetpoint != self.curSetpoint):
+            #new setpoint, need to recalculate trajectory
+            profile = TrapezoidProfile(self.constraints,curSetpoint,self.prevProfiledSetpoint)
+            self.profileStartTime = curTime
 
         curCmdState = profile.calculate(curTime - self.profileStartTime)
-
         # Use curCmdState.velocity and .position for feedforward and feedback motor control
 
-
         self.prevCmdState = curCmdState
+        """
+
+        #rot
+
     """
 
     def LinearDispFromMotorRev_SingerUpDown(self):
@@ -55,6 +61,6 @@ class CarriageControl:
         return self.motorRotations
     
 
-    """will need to get command position from the operator controller and
+    will need to get command position from the operator controller and
     desired singer angle for autolign"""
     
