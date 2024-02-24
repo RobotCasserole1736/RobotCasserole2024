871308da515aeccc60ca5cd5458d2bc4a7990cd1
Elliot Watkins <frc1736student@gmail.com>
2024-02-24 11:13:40 -0600

    Removed amp auto align as it was causing problems

--- drivetrain/controlStrategies/autoDrive.py
+++ drivetrain/controlStrategies/autoDrive.py
@@ -16,7 +16,6 @@ from humanInterface.ledControl import LEDControl
 class AutoDrive(metaclass=Singleton):
     def __init__(self):
         self.speakerAlignActive = False
-        self.ampAlignActive = False
         self.returnDriveTrainCommand = DrivetrainCommand()
         self.rotKp = Calibration("Auto Align Rotation Kp",2)
         self.rotKd = Calibration("Auto Align Rotation Kd",3)
@@ -52,10 +51,6 @@ class AutoDrive(metaclass=Singleton):
             self.ledCtrl.setSpeakerAutoAlignActive(True)
             self.getDesiredSingerAngle(curPose)
             return self.calcSpeakerDrivetrainCommand(curPose, cmdIn)
-        elif self.ampAlignActive:
-            self.ledCtrl.setSpeakerAutoAlignActive(False)
-            self.ledCtrl.setAmpAutoAlignActive(True)
-            return self.calcAmpDrivetrainCommand(curPose, cmdIn)
         else:
             self.ledCtrl.setSpeakerAutoAlignActive(False)
             return cmdIn
@@ -107,8 +102,4 @@ class AutoDrive(metaclass=Singleton):
         self.returnDriveTrainCommand.velT = rotError*self.rotKp.get() + velTCmdDer*self.rotKd.get()
         self.returnDriveTrainCommand.velX = cmdIn.velX # Set the X vel to the original X vel
         self.returnDriveTrainCommand.velY = cmdIn.velY # Set the Y vel to the original Y vel
-        return self.returnDriveTrainCommand
-    
-    def calcAmpDrivetrainCommand(self, curPose: Pose2d, cmdIn: DrivetrainCommand) -> DrivetrainCommand:
-        
         return self.returnDriveTrainCommand
