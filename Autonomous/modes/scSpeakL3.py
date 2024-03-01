818561412612e86e4bcce97b99644322f68611b8
Qbert <frc1736student@gmail.com>
2024-02-29 21:06:27 -0600

    Working on wrangling the auto sequencer

--- Autonomous/modes/scSpeakL3.py
+++ Autonomous/modes/scSpeakL3.py
@@ -1,5 +1,7 @@
 from AutoSequencerV2.mode import Mode
+from AutoSequencerV2.raceCommandGroup import RaceCommandGroup
 from Autonomous.commands.drivePathCommand import DrivePathCommand
+from Autonomous.commands.intakeCommand import IntakeCommand
 from Autonomous.commands.speakerShootCommand import SpeakerShootCommand
 from AutoSequencerV2.sequentialCommandGroup import SequentialCommandGroup
 
@@ -8,12 +10,16 @@ class ScSpeakL3(Mode):
         Mode.__init__(self, f"Sc Speak L 3")
         self.SequentialCommandGroup = SequentialCommandGroup()
         self.pathCmd = DrivePathCommand("DriveOut3")
+        self.intake = IntakeCommand()
         self.shoot = SpeakerShootCommand()
         self.commandList = [self.shoot, self.pathCmd]
+        self.commandList2 = SequentialCommandGroup(self.commandList)
+        self.extraList = [self.intake, self.shoot]
+        self.extraList2 = RaceCommandGroup(self.extraList)
     
     def getCmdGroup(self):
         # Return shoot, then path command
-        return SequentialCommandGroup(self.commandList)
+        return self.commandList2.andThen(self.intake)
 
     def getInitialDrivetrainPose(self):
         # Use the path command to specify the starting pose
