8fd35e0d188eb012443d724a568c9fe3cb3e7d7e
Elliot Watkins <frc1736student@gmail.com>
2024-03-02 12:27:15 -0600

    Deleted unused member from robotIdentification

--- utils/robotIdentification.py
+++ utils/robotIdentification.py
@@ -12,7 +12,6 @@ class RobotIdentification(metaclass=Singleton):
         self.roboControl = wpilib.RobotController
         self.robotType = RobotTypes.Main
         self.serialFault = False
-        self.robotTypeNumber = 0
 
     def configureValue(self):
 
@@ -20,20 +19,16 @@ class RobotIdentification(metaclass=Singleton):
 
         if self.roboControl.getSerialNumber() == "03134d41": #the L3
             self.robotType = RobotTypes.Main 
-            self.robotTypeNumber = 1
         elif self.roboControl.getSerialNumber() == "03064e3f":  #the L2
             self.robotType = RobotTypes.Practice
-            self.robotTypeNumber = 2
         elif self.roboControl.getSerialNumber() == "0316b37c":  
             #Test to see if the RoboRio serial number is our testboard's serial number.
             self.robotType = RobotTypes.TestBoard
-            self.robotTypeNumber = 3
         else:
             #If the Robo Rio's serial number is not equal to any of our known serial numbers, 
             #assume we are the main robot
             self.robotType = RobotTypes.Main
             self.serialFault = True
-            self.robotTypeNumber = 0
 
         #I don't know why the logs aren't working. But it's printing so I give up. 
 
@@ -45,6 +40,3 @@ class RobotIdentification(metaclass=Singleton):
 
     def getSerialFaulted(self):
         return self.serialFault
-
-    def getRobotTypeNumber(self):
-        return self.robotTypeNumber
