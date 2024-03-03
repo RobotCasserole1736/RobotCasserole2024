12877ed7f9dbcc596afe49b02e79b42a577118cb
ElliottHicks1125 <elliott41414@gmail.com>
2024-03-02 20:13:57 -0600

    Created auto path and mode for scoring two notes. Seems like the intake is running when it shouldn't be in sim. Will do more debugging on robot.

---
+++ Autonomous/modes/scoreTwo.py
@@ -0,0 +1,28 @@
+from AutoSequencerV2.raceCommandGroup import RaceCommandGroup
+from AutoSequencerV2.builtInCommands.waitCommand import WaitCommand
+from AutoSequencerV2.sequentialCommandGroup import SequentialCommandGroup
+from Autonomous.commands.drivePathCommand import DrivePathCommand
+from AutoSequencerV2.mode import Mode
+from Autonomous.commands.intakeCommand import IntakeCommand
+from Autonomous.commands.speakerShootCommand import SpeakerShootCommand
+
+
+# Just drives out of the starting zone. That's all.
+class scoreTwo(Mode):
+    def __init__(self):
+        Mode.__init__(self, f"Score Two")
+        self.pathCmd = DrivePathCommand("scoreTwo")
+        self.intake = IntakeCommand()
+        self.shoot = SpeakerShootCommand()
+        self.wait = WaitCommand(3)
+        
+        self.intakeCommandList = [self.wait, self.pathCmd, self.intake]
+        self.intakeCommandGroup = RaceCommandGroup(self.intakeCommandList)
+        
+    def getCmdGroup(self):
+        # Just return the path command
+        return self.shoot.andThen(self.intakeCommandGroup).andThen(self.shoot)
+
+    def getInitialDrivetrainPose(self):
+        # Use the path command to specify the starting pose
+        return self.pathCmd.path.getInitialPose()
