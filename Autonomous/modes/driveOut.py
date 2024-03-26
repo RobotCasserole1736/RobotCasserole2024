9e3d1204faefc38f9379f99e53defd40e27b11c7
ElliottHicks1125 <elliott41414@gmail.com>
2024-03-26 17:42:22 -0500

    little fix

--- Autonomous/modes/driveOut.py
+++ Autonomous/modes/driveOut.py
@@ -6,7 +6,7 @@ from AutoSequencerV2.mode import Mode
 class DriveOut(Mode):
     def __init__(self):
         Mode.__init__(self, f"Drive Out")
-        self.pathCmd = DrivePathCommand("outAndBack")
+        self.pathCmd = DrivePathCommand("DriveOut")
         #self.pathCmd = DrivePathCommand("rotate")
 
     def getCmdGroup(self):
