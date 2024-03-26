ef243c3e2e167f8128e0de5d666899cb45f3cba9
Elliot Watkins <frc1736student@gmail.com>
2024-03-26 17:33:21 -0500

    Clean up for climber control class

--- climbControl/climberControl.py
+++ climbControl/climberControl.py
@@ -1,29 +1,17 @@
 from rev import CANSparkMax, CANSparkLowLevel
 from wpilib import Relay
-from wpimath.filter import Debouncer
 from utils.constants import CLIMBER_MOTOR_LEFT_CANID, CLIMBER_MOTOR_RIGHT_CANID
-from utils.calibration import Calibration
 from wrappers.wrapperedSparkMax import WrapperedSparkMax
-
-# needs a sparkmax, relay thing
-# the relay thing(?) should basically be a solenoid that automatically 
-# braces/locks/something so we stay up when the time is done
 
 class ClimberControl:
     def __init__(self):
         self.ratchetLeft = Relay(0, Relay.Direction.kBothDirections)
         self.ratchetRight = Relay(1, Relay.Direction.kBothDirections)
-
         self.winchLeft = WrapperedSparkMax(CLIMBER_MOTOR_LEFT_CANID, "_winch")
         self.winchRight = CANSparkMax(CLIMBER_MOTOR_RIGHT_CANID,CANSparkLowLevel.MotorType.kBrushless)
         self.winchRight.setIdleMode(CANSparkMax.IdleMode.kBrake)
         # Set the Right winch to follow the left and invert it
         self.winchRight.follow(self.winchLeft.ctrl, True)
-
-        self.ratchetDebouncerTime = Calibration("RachetDebounce", 0.040, "Seconds")
-        self.ratchetDebouncer = Debouncer(
-            self.ratchetDebouncerTime.get(), Debouncer.DebounceType.kRising
-        )
         self.cmdVol = 0
 
     def ctrlWinch(self, cmdIn):
